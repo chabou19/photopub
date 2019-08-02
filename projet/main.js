a54c1cc1ab3aa304cca0f221630531836653fe22
chabou19 <52921032+chabou19@users.noreply.github.com>
2019-08-02 14:25:49 +0100

    Add files via upload

---
+++ projet/main.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD5zn7HuNRcSmbxd4o4GDaBLYKLq6zUzCM",
+    authDomain: "fir-96f3a.firebaseapp.com",
+    databaseURL: "https://fir-96f3a.firebaseio.com",
+    projectId: "fir-96f3a",
+    storageBucket: "",
+    messagingSenderId: "496193824628",
+    appId: "1:496193824628:web:9a5caafdd8dab819"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+  
